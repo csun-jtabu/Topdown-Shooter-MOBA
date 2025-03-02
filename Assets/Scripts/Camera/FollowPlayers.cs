0babecf0e9df0c80365e9f56a1b6bc21a8ca1bf2
JKosof <34256205+JKosof@users.noreply.github.com>
2025-03-02 13:39:48 -0800

    Fixed compiler issue with camera script

--- Assets/Scripts/Camera/FollowPlayers.cs
+++ Assets/Scripts/Camera/FollowPlayers.cs
@@ -1,18 +1,18 @@
 using System.Numerics;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class FollowPlayers : MonoBehaviour
 {
     [SerializeField]
     private float _followSpeed = 2f; // this is the speed the camera will follow
     [SerializeField]
-    private Transform target; // this is what we'll be tracking
+    private Transform targetPlayer; // this is what we'll be tracking
 
     // Update is called once per frame
     void Update()
     {
         // basically gets the player's position
-        UnityEngine.Vector3 newPosition = new UnityEngine.Vector3(target.position.x, target.position.y, -10);
+        UnityEngine.Vector3 newPosition = new UnityEngine.Vector3(targetPlayer.position.x, targetPlayer.position.y, -10);
         // basically moves the camera to the player
         transform.position = newPosition;
     }
