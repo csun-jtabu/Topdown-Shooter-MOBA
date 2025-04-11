b6651c8fb642d46df23d2c02a4b30bff25677d07
Rahi <125520697+RahiMisra@users.noreply.github.com>
2025-04-10 23:02:47 -0700

    Menu Starts Game

--- Assets/Scripts/Menu/MainMenuScript.cs
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -6,7 +6,7 @@ public class MainMenuScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
